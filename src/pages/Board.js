5160f3757c584fc5bf45f341b8900d40db9fffa7
QTSYQ <50006931+QTSYQ@users.noreply.github.com>
2022-07-15 05:33:23 +0900

    add : board 기본 골자 추가
    ㅇㅇ

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,11 +1,11 @@
 import Footer from "../components/common/Footer.js";
 import Header from "../components/common/Header.js";
-
+import Post from "../components/post/Post.js";
 function Board() {
   return (
     <div>
       <Header />
-
+      <Post />
       <Footer />
     </div>
   );
